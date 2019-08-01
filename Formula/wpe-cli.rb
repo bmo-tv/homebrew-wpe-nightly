ff2e02f0507ea748eafff9cdf6f6719eed7c7a59
Ben Moore <benjamin.moore@wpengine.com>
2019-08-01 10:29:58 -0500

    Static nightly devel path

--- Formula/wpe-cli.rb
+++ Formula/wpe-cli.rb
@@ -12,9 +12,9 @@ class WpeCli < Formula
     url "https://s3-us-east-2.amazonaws.com/wpeclibinaries/wpe-cli/v0.13.9/wpe-cli_0.13.9_linux_amd64.tar.gz"
     sha256 "52a89251ff19a808661a363fbff8bb6f3a41acf60d09d26eaa65438d838c1d54"
   end
-  
+
   devel do
-    url "https://s3-us-east-2.amazonaws.com/wpeclinightly/wpe-cli/0.13.9/wpe-cli_0.13.9_darwin_amd64.tar.gz"
+    url "https://s3-us-east-2.amazonaws.com/wpeclinightly/nightly/wpe-cli_nightly_darwin_amd64.tar.gz"
   end
 
   def install
